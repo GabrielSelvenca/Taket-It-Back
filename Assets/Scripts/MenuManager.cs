6cf9ca30466dd46322755831b37831bd18c7ea89
MarkPSM <markins.psm@gmail.com>
2025-07-31 23:53:13 -0300

    feat: Mapa, CursorChanger; Fix: GameData, WrapperJsons, PlayerMovements, MenuManagment

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -2,13 +2,31 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
 using Unity.VisualScripting;
+using TMPro;
+using Assets.Scripts;
 
 public class MenuManager : MonoBehaviour
 {
+
+    [Header("Menu")]
     public Canvas canvasMenu;
     public Image painelFade;
-    public Button startButton;
     public float duracaoFade = 1.5f;
+
+    [Header("Panel")]
+    public GameObject Panel;
+    public TMP_InputField InputName;
+    public GameData gameData;
+
+    [Header("Player")] //Perdão pela gambiarra ;-;
+    public GameObject Player;
+
+    private void Awake()
+    {
+        Panel.SetActive(false);
+
+        Player.gameObject.SetActive(false);
+    }
 
     private void Start()
     {
@@ -18,10 +36,32 @@ public class MenuManager : MonoBehaviour
         }
     }
 
+    public void BringMenu()
+    {
+        if (Panel != null)
+        {
+            Panel.SetActive (true);
+        }
+    }
+
     public void Jogar()
     {
+        string nomeJogador = InputName.text.Trim();
+
+        if (nomeJogador != null)
+        {
+            gameData.InserirRanking(nomeJogador, 0);
+            Debug.Log($"O nome do jogador é {nomeJogador}");
+        }
+        else
+        {
+            Debug.LogWarning("Nome do jogador está vazio.");
+            return;
+        }
+
         StartCoroutine(FadeOutMenu());
     }
+
 
     IEnumerator FadeInMenu()
     {
@@ -39,10 +79,13 @@ public class MenuManager : MonoBehaviour
         }
 
         painelFade.color = corFinal;
+        painelFade.enabled = false;
     }
 
     IEnumerator FadeOutMenu()
     {
+        painelFade.enabled = true;
+
         float tempo = 0f;
         Color corInicial = painelFade.color;
         Color corFinal = new Color(0, 0, 0, 1f);
@@ -55,6 +98,7 @@ public class MenuManager : MonoBehaviour
 
         painelFade.color = corFinal;
         canvasMenu.gameObject.SetActive(false);
+        Player.gameObject.SetActive(true);
     }
 
     public void Tutorial()
