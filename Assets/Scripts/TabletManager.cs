ff9ae0b40004b8e781ec30133d74b5936525bb71
GabrielSelvenca <gabriel.selvenca@icloud.com>
2025-08-03 17:39:56 -0300

    feat Subindo TabletManager

--- Assets/Scripts/TabletManager.cs
+++ Assets/Scripts/TabletManager.cs
@@ -1,18 +1,52 @@
+using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TabletManager : MonoBehaviour
 {
+    public TMP_InputField inputId;
+    public Button btnBuscar;
+    public RectTransform errorPopUp;
+    private List<FuncionarioConsumivel> listaConsumiveisFuncionario;
+    private Funcionarios funcionario;
 
-
-
-    void Start()
+    public void Buscar()
     {
-        
+        if (int.TryParse(inputId.text.Trim(), out int id))
+        {
+            funcionario = GameData.Instance.GetFuncionario(id);
+            listaConsumiveisFuncionario = GameData.Instance.GetConsFunc(id);
+
+            if (funcionario != null && listaConsumiveisFuncionario != null && listaConsumiveisFuncionario.Count > 0)
+            {
+                Debug.Log($"Funcionario {funcionario.nome} encontrado com {listaConsumiveisFuncionario.Count} consumiveis.");
+            }
+            else
+            {
+                Debug.LogWarning("Funcionário não encontrado ou sem consumíveis.");
+                ErrorPopUp();
+            }
+        }
+        else
+        {
+            Debug.LogWarning("ID inválido");
+            ErrorPopUp();
+        }
     }
 
-
-    void Update()
+    public void ErrorPopUp()
     {
-        
+        errorPopUp.gameObject.SetActive(true);
+        btnBuscar.gameObject.SetActive(false);
+        StartCoroutine(ErrorPopOut());
+    }
+
+    private IEnumerator ErrorPopOut()
+    {
+        yield return new WaitForSeconds(5);
+        errorPopUp.gameObject.SetActive(false);
     }
 }
